7a11a2fc8d9e707c588193ea265e157e1ee39c68
Goran Smiljanic <sgoran87@gmail.com>
2015-05-31 21:42:47 +0200

    Changed build output

--- build/temp/production/basic/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-Compass.add_project_configuration('..\..\..\..\..\basic\sass\config.rb')
-cache_path = 'C:\wamp\www\espace/build/.sass-cache'
--- build/temp/production/basic/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\wamp\www\espace\basic\app.js
